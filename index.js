3af39f6a56f30c51b22165ccd944f013929fb962
AdrSpeer <adn.speer@gmail.com>
2024-08-03 11:20:42 +0200

    test js

---
+++ index.js
@@ -0,0 +1,5 @@
+const numbers = [5, 6, 7, 8, 12, 25, 104, 45, 678, 128];
+
+const filteredNumber = numbers.filter((number)=> number > 10);
+
+const sortedNumber = filteredNumber.sort((a,b) => a - b);
